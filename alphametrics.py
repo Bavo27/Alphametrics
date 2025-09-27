4f9563be3e7f7fbc4c3019a091b108eaf763e19d
Bavo27 <bvandenh@macalester.edu>
2025-09-26 21:57:32 -0400

    finished

--- alphametrics.py
+++ alphametrics.py
@@ -3,6 +3,7 @@ from itertools import permutations
 def alphametics():
     wordList = ["saturn", "uranus", "neptune", "pluto", "planets"]
     letterList = ["s", "p", "n", "u", "a", "t", "r", "e", "o", "l"]
+    leadingLetters = ["s", "p", "n", "u"]
     digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     allDigitsPermutations = permutations(digits)
 
@@ -10,14 +11,11 @@ def alphametics():
         currList = i
         if currList[0] == 0 or currList[1] == 0 or currList[2] == 0 or currList[3] == 0:
                 continue
-        setLetters = {}
-        # setLetters = {letterList[k]: currList[k] for k in range(len(letterList))}
         numbers = []
-        for j in range(len(currList)):
-            setLetters.setdefault(letterList[j], currList[j])
+        setLetters = {letterList[j]: currList[j] for j in range(len(letterList))}
+
         for w in wordList:
-            if w == "planets":
-                break
+
             multiplier = 0
             value = 0
             
@@ -28,6 +26,6 @@ def alphametics():
         
         total = sum(numbers[:-1])
         if total == numbers[len(numbers)-1]:
-            return setLetters
+            return numbers, setLetters
 
 print(alphametics())
