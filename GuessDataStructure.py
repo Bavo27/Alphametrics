eb0af483727efbdb9bceee98f45def3f9ce2b56c
Bavo27 <bvandenh@macalester.edu>
2025-10-06 11:48:38 -0400

    kattis homework 3

---
+++ GuessDataStructure.py
@@ -0,0 +1,45 @@
+import sys
+from collections import deque
+import heapq
+
+def guessTheDataStructure():
+    answerList = []
+    for line in sys.stdin: # to loop over the inputs repeatedly
+        numTimes = int(line.strip()) # read in number of lines
+        stack = []
+        queue = deque()
+        priority_queue = []
+        isStack = True
+        isQueue = True
+        isPriority = True
+        for i in range(numTimes): # loop over that many times
+            x,y = input().split()
+            x = int(x.strip())
+            y = int(y.strip())
+            if x == 1:
+                stack.append(y)
+                queue.append(y)
+                heapq.heappush(priority_queue, -y)
+            elif x == 2:
+                if isStack:
+                    if len(stack) == 0 or stack.pop() != y:
+                        isStack = False
+                if isQueue:
+                    if len(queue) == 0 or queue.popleft() != y:
+                        isQueue = False
+                if isPriority:
+                    if len(priority_queue) == 0 or abs(heapq.heappop(priority_queue)) != y:
+                        isPriority = False
+            
+        if isStack == False and isQueue == False and isPriority == False:
+            answerList.append("impossible")
+        elif isStack == True and (isQueue == False and isPriority == False):
+            answerList.append("stack")
+        elif isQueue == True and (isStack == False and isPriority == False):
+            answerList.append("queue")
+        elif isPriority == True and (isStack == False and isQueue == False):
+            answerList.append("priority queue")
+        else:
+            answerList.append("not sure")
+    for response in answerList:
+        print(response)
